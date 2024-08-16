522ac770b07b989d3ef96a823541685bf0e5fc15
Gaffeecate <unitnatis123@gmail.com>
2024-08-16 14:16:21 +0900

    servo moves per second

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -2,30 +2,27 @@
 
 Servo myservo;
 int servoPin = 9; // 서보 모터 연결 핀
+int angle = 0; // 시작 각도
+unsigned long previousMillis = 0; // 이전에 각도를 변경한 시각을 저장
+const long interval = 3000; // 각도 변경 간격 (밀리초 단위)
 
 void setup() {
   myservo.attach(servoPin);
   Serial.begin(9600);
-  Serial.println("Enter angle (0-180):");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    int angle = Serial.parseInt();
-    
-    if (angle >= 0 && angle <= 180) {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if (angle <= 180) {
       myservo.write(angle);
       Serial.print("Servo rotated to ");
       Serial.print(angle);
       Serial.println(" degrees");
+      angle += 10; // 각도를 10도 증가
     } else {
-      Serial.println("Invalid angle. Enter 0-180");
+      angle = 0; // 각도가 180도를 초과하면 0도로 초기화
     }
-    
-    while (Serial.available() > 0) {
-      Serial.read();
-    }
-    
-    Serial.println("Enter next angle (0-180):");
   }
 }
